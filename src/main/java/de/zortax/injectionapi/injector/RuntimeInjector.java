abf0d1abfd5331133687c1e3d5bc36cf58055d61
Zortax <leonard.seibold.m@gmail.com>
2018-05-27 17:09:03 +0200

    basic project setup

--- src/main/java/de/zortax/injectionapi/injector/RuntimeInjector.java
+++ src/main/java/de/zortax/injectionapi/injector/RuntimeInjector.java
@@ -1,9 +1,11 @@
 package de.zortax.injectionapi.injector;// Created by leo on 27.05.18
 
+import com.sun.tools.attach.VirtualMachine;
+
 public class RuntimeInjector {
 
     public static void main(String[] args) {
-
+        VirtualMachine m;
     }
 
 }
