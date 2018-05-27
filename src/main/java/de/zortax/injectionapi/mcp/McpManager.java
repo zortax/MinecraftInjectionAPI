e99aa270bdc1ef72d81e5ec50554c2ef8d12f9ef
Zortax <leonard.seibold.m@gmail.com>
2018-05-27 18:25:04 +0200

    unzip mappings

--- src/main/java/de/zortax/injectionapi/mcp/McpManager.java
+++ src/main/java/de/zortax/injectionapi/mcp/McpManager.java
@@ -1,10 +1,14 @@
 package de.zortax.injectionapi.mcp;// Created by leo on 27.05.18
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class McpManager {
 
@@ -18,6 +22,13 @@ public class McpManager {
     }
 
     public static void downloadMappings(String version, String mcpMappingLink, String mcpSrgLink) {
+
+        File f = new File("mappings/" + version + "/");
+        if (f.exists() && f.isDirectory()) {
+            System.out.println("Mappings already downloaded... [SKIPPED]");
+            return;
+        }
+
         try {
 
             System.out.println("Downloading MCP mappings from " + mcpMappingLink + " ...");
@@ -32,8 +43,80 @@ public class McpManager {
             fos = new FileOutputStream("srg-" + version + ".zip");
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
 
+            File mappingsDir = new File("mappings/" + version + "/");
+            if (!mappingsDir.exists() || !mappingsDir.isDirectory()) {
+                if (!mappingsDir.mkdirs()) {
+                    System.out.println("Couldn't create mappings directory...");
+                    return;
+                }
+            }
+
+            unzipFile("mcp-" + version + ".zip", "mappings/" + version + "/mcp/");
+            unzipFile("srg-" + version + ".zip", "mappings/" + version + "/srg/");
+
+            System.out.println("Deleting downloaded ZIP files...");
+
+            File mcpZip = new File("mcp-" + version + ".zip");
+            File srgZip = new File("srg-" + version + ".zip");
+            mcpZip.delete();
+            srgZip.delete();
+
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        System.out.println("Done!");
+    }
+
+    private static void unzipFile(String zipFile, String outputFolder){
+
+        System.out.println("Unzipping " + zipFile + " ...");
+
+        byte[] buffer = new byte[1024];
+
+        try{
+
+            File folder = new File(outputFolder);
+            if(!folder.exists()){
+                if (!folder.mkdir()) {
+                    System.out.println("Couldn't create mappings directory...");
+                    return;
+                }
+            }
+
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+            ZipEntry ze = zis.getNextEntry();
+
+            while(ze!=null){
+
+                if (ze.isDirectory()) {
+                    ze = zis.getNextEntry();
+                    continue;
+                }
+
+                String fileName = ze.getName();
+                File newFile = new File(outputFolder + File.separator + fileName);
+
+                System.out.println("Extracting "+ newFile.getPath() + " ...");
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fos = new FileOutputStream(newFile);
+
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+
+                fos.close();
+                ze = zis.getNextEntry();
+            }
+
+            zis.closeEntry();
+            zis.close();
+
+            System.out.println("Done unzipping " + zipFile);
+
+        }catch(IOException ex){
+            ex.printStackTrace();
         }
     }
 
